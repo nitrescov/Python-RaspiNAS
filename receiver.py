731b31db10e8f4086a4ef1aa1a528a9c7c895114
nitrescov <nitrescov@protonmail.com>
2022-05-08 00:41:16 +0200

    Add packet headers for Socket/TCP communication

--- receiver.py
+++ receiver.py
@@ -15,6 +15,7 @@
 
 import os
 import socket
+import struct
 import threading
 from random import randint
 
@@ -33,13 +34,19 @@ def socket_server(host_ip, port, usernames, userdata):
 def handle_connection(connection, usernames, userdata):
     separator = "<separator>"
     buffer = 65536  # 64K TCP packet size
-    frame_counters = [str(randint(n*42, n*42+42)) for n in range(6)]
-    fc_list = ",".join(frame_counters)
+    frame_counters = [randint(n*42, n*42+42) for n in range(6)]
+    fc_list = ",".join([str(m) for m in frame_counters])
 
     try:
-        connection.sendall((frame_counters[0] + separator + fc_list).encode())
-        login_packet = connection.recv(buffer).decode()
-        fc1, name, hash_value = login_packet.split(separator)
+        fc_packet = fc_list.encode()
+        send_header(connection, len(fc_packet), frame_counters[0])
+        connection.sendall(fc_packet)
+
+        login_len, fc1 = recv_header(connection)
+        if not login_len:
+            raise ValueError("no packet data")
+        login_packet = connection.recv(login_len).decode()
+        name, hash_value = login_packet.split(separator)
         name_position = -1
         hash_position = -1
         for i in range(len(usernames)):
@@ -48,7 +55,7 @@ def handle_connection(connection, usernames, userdata):
         for j in range(len(userdata)):
             if hash_value == userdata[j]:
                 hash_position = j
-        if (fc1 == frame_counters[1]) and (name_position >= 0) and (hash_position >= 0) and (name_position == hash_position):
+        if fc1 == frame_counters[1] and 0 <= name_position == hash_position >= 0:
             folder = ""
             for root, dirs, files in os.walk(f"users/{name}"):
                 folder_path = root[6:]
@@ -58,33 +65,57 @@ def handle_connection(connection, usernames, userdata):
                 else:
                     folder = folder + "\n" + ("  " * (subdir_count - 1)) + folder_path
 
-            folder_bytes = (frame_counters[2] + separator + folder).encode()
-            while True:
-                byte_block = folder_bytes[:buffer]
-                if not byte_block:
-                    break
-                connection.sendall(byte_block)
-                folder_bytes = folder_bytes[buffer:]
+            folder_packet = folder.encode()
+            send_header(connection, len(folder_packet), frame_counters[2])
+            connection.sendall(folder_packet)
 
-            file_info = connection.recv(buffer).decode()
-            fc3, file_name, file_size, path = file_info.split(separator)
+            file_info_len, fc3 = recv_header(connection)
+            if not file_info_len:
+                raise ValueError("no packet data")
+            file_info_packet = connection.recv(file_info_len).decode()
+            file_name, file_size, path = file_info_packet.split(separator)
             file_name = os.path.basename(file_name)
 
             if fc3 == frame_counters[3] and os.path.isdir(f"users/{path}") and not os.path.isfile(f"users/{path}/{file_name}"):
-                connection.sendall((frame_counters[4] + separator + "True").encode())
+                confirm_packet = "True".encode()
+                send_header(connection, len(confirm_packet), frame_counters[4])
+                connection.sendall(confirm_packet)
+
+                file_len, fc5 = recv_header(connection)
+                if not file_len:
+                    raise ValueError("no packet data")
+                if fc5 != frame_counters[5]:
+                    raise ValueError("invalid packet data")
+                max_packets = int(file_len / buffer) + 1
+                packet_count = 0
                 new_file = open(f"users/{path}/{file_name}", "wb")
-                while True:
+                while packet_count < max_packets:
                     file_bytes = connection.recv(buffer)
                     if not file_bytes:
                         break
                     new_file.write(file_bytes)
+                    packet_count += 1
                 new_file.close()
             else:
-                connection.sendall((frame_counters[4] + separator + "False").encode())
+                refuse_packet = "False".encode()
+                send_header(connection, len(refuse_packet), frame_counters[4])
+                connection.sendall(refuse_packet)
             connection.close()
         else:
-            connection.sendall((frame_counters[2] + separator + "False").encode())
+            refuse_packet = "False".encode()
+            send_header(connection, len(refuse_packet), frame_counters[2])
+            connection.sendall(refuse_packet)
             connection.close()
     except ValueError:
-        print("[SOCKET LOG] Possible security risk detected: received data with wrong number of arguments.")
+        print("[SOCKET LOG] Possible security risk detected: Received no or invalid data.")
         connection.close()
+
+def send_header(sock: socket.socket, msg_len: int, fc: int):
+    msg_header = struct.pack("!Q", msg_len) + struct.pack("!H", fc)
+    sock.sendall(msg_header)
+
+def recv_header(sock: socket.socket):
+    raw_header = sock.recv(10)
+    if not raw_header:
+        return 0, 0
+    return struct.unpack("!Q", raw_header[:8])[0], struct.unpack("!H", raw_header[8:])[0]
